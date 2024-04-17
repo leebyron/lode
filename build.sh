71643209676fd1fee7209ecb687a5ea72262a0d5
Lee Byron <lee@leebyron.com>
2024-04-17 22:30:14 +0000

    fixes

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-git submodule update --init --depth 1 iosevka
 pushd iosevka
+git restore .
 git apply --check ../iosevka.patch 2>/dev/null && git apply ../iosevka.patch
 ln -fs ../private-build-plans.toml private-build-plans.toml
 npm install
