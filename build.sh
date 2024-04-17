5e27c61cfb921b0cf5b146467db39c2c97ae072e
Lee Byron <lee@leebyron.com>
2024-04-17 14:58:41 -0700

    test local patches

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 git submodule update --init --depth 1 iosevka
 pushd iosevka
+git apply --check ../iosevka.patch 2>/dev/null && git apply ../iosevka.patch
 ln -fsw ../private-build-plans.toml private-build-plans.toml
 npm install
 npm run build -- super-ttc::Lode
