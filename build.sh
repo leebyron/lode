1ec9c9d623beceaa02a73b123ad53484e267618e
Lee Byron <lee@leebyron.com>
2024-04-18 16:26:50 -0700

    wip

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 pushd iosevka
-git restore .
-git apply ../iosevka.patch
-ln -fs ../private-build-plans.toml private-build-plans.toml
-npm install
+# git restore .
+# git apply ../iosevka.patch
+# ln -fs ../private-build-plans.toml private-build-plans.toml
+# npm install
 npm run build -- super-ttc::Lode
+# npm run build -- ttc::Lode
